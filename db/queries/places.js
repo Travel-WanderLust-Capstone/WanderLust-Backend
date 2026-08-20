cf36f45ede2d7f0412618138fe5bd3b9cae96072
Asher Pardel <gracepardel7@gmail.com>
2026-08-19 20:46:47 -0400

    finished backend for explore pages

---
+++ db/queries/places.js
@@ -0,0 +1,25 @@
+import db from "#db/client";
+
+export async function getPlacesByLocation(location_id) {
+  const sql = `SELECT name FROM place WHERE location_id = $1;`;
+  const { rows } = await db.query(sql, [location_id]);
+  return rows;
+}
+
+export async function getActivitiesByLocation(location_id) {
+  const sql = `SELECT name FROM place WHERE location_id = $1 AND type = $2;`;
+  const { rows } = await db.query(sql, [location_id, "activity"]);
+  return rows;
+}
+
+export async function getLodgingByLocation(location_id) {
+  const sql = `SELECT name FROM place WHERE location_id = $1 AND type = $2;`;
+  const { rows } = await db.query(sql, [location_id, "lodging"]);
+  return rows;
+}
+
+export async function getPlaceDetails(place_id) {
+  const sql = `SELECT * FROM place WHERE id = $1;`;
+  const { rows } = await db.query(sql, [place_id]);
+  return rows;
+}
--- db/queries/selections.js
+++
@@ -1,5 +0,0 @@
-import db from "#db/client";
-
-export async function getSelections() {
-  const sql = `SELECT * FROM selections`;
-}
