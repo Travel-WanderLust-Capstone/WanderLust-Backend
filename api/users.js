7dd62fe5185acd58eef34810e24113c1e910d4f9
Asher Pardel <gracepardel7@gmail.com>
2026-08-30 18:46:42 -0400

    users queries and api update here. idk wtf happened to the files and im just gonna re download the files bc i am tired and on time crunch (#64)

--- api/users.js
+++ api/users.js
@@ -1,28 +1,30 @@
 import express from "express";
 const router = express.Router();
 export default router;
-
-import { createUser, getUserByUsernameAndPassword } from "#db/queries/users";
+import { createUser, getUserByEmailAndPassword } from "#db/queries/users";
 import requireBody from "#middleware/requireBody";
 import { createToken } from "#utils/jwt";
 
 router
   .route("/register")
-  .post(requireBody(["username", "password"]), async (req, res) => {
-    const { username, password } = req.body;
-    const user = await createUser(username, password);
-
-    const token = await createToken({ id: user.id });
+  .post(requireBody(["name", "email", "password"]), async (req, res) => {
+    const { name, email, password } = req.body;
+    const user = await createUser(name, email, password);
+    const token = createToken({
+      id: user.id,
+    });
     res.status(201).send(token);
   });
-
 router
   .route("/login")
-  .post(requireBody(["username", "password"]), async (req, res) => {
-    const { username, password } = req.body;
-    const user = await getUserByUsernameAndPassword(username, password);
-    if (!user) return res.status(401).send("Invalid username or password.");
-
-    const token = await createToken({ id: user.id });
+  .post(requireBody(["email", "password"]), async (req, res) => {
+    const { email, password } = req.body;
+    const user = await getUserByEmailAndPassword(email, password);
+    if (!user) {
+      return res.status(401).send("Invalid email or password.");
+    }
+    const token = createToken({
+      id: user.id,
+    });
     res.send(token);
   });
