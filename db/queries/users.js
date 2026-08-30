d2f0321b0eb8a3b056bc6a2e1d546d64843e48ce
Asher Pardel <gracepardel7@gmail.com>
2026-08-30 18:14:57 -0400

    users queries and api update here. idk wtf happened to the files and im just gonna re download the files bc i am tired and on time crunch

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,33 +1,39 @@
 import db from "#db/client";
 import bcrypt from "bcrypt";
 
-export async function createUser(username, password) {
+export async function createUser(name, email, password) {
   const sql = `
-  INSERT INTO users
-    (username, password)
-  VALUES
-    ($1, $2)
-  RETURNING *
+    INSERT INTO users
+      (name, email, password)
+    VALUES
+      ($1, $2, $3)
+    RETURNING id, name, email
   `;
+
   const hashedPassword = await bcrypt.hash(password, 10);
+
   const {
     rows: [user],
-  } = await db.query(sql, [username, hashedPassword]);
+  } = await db.query(sql, [name, email, hashedPassword]);
+
   return user;
 }
 
-export async function getUserByUsernameAndPassword(username, password) {
+export async function getUserByEmailAndPassword(email, password) {
   const sql = `
-  SELECT *
-  FROM users
-  WHERE username = $1
+    SELECT *
+    FROM users
+    WHERE email = $1
   `;
+
   const {
     rows: [user],
-  } = await db.query(sql, [username]);
+  } = await db.query(sql, [email]);
+
   if (!user) return null;
 
   const isValid = await bcrypt.compare(password, user.password);
+
   if (!isValid) return null;
 
   return user;
@@ -35,12 +41,15 @@ export async function getUserByUsernameAndPassword(username, password) {
 
 export async function getUserById(id) {
   const sql = `
-  SELECT *
-  FROM users
-  WHERE id = $1
+    SELECT *
+    FROM users
+    WHERE id = $1
   `;
+
   const {
     rows: [user],
   } = await db.query(sql, [id]);
+
   return user;
 }
+``;
